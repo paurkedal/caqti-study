4b1f54d8ab5b6890d22d215aa2217b8d8823c6eb
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-05-01 16:57:34 +0200

    Update deploy steps

--- DEPLOY.md
+++ DEPLOY.md
@@ -26,16 +26,14 @@ This command generates static content into the `build` directory and can be serv
 
 ### Deployment
 
-Using SSH:
+Change: `./docusaurus.config.ts`
+
+`organizationName` + `projectName` = github.com/organizationName/projectName
+
+Then deploy:
 
 ```
-$ USE_SSH=true yarn deploy
-```
-
-Not using SSH:
-
-```
-$ GIT_USER=<Your GitHub username> yarn deploy
+$ USE_SSH=true npm run deploy
 ```
 
 If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,15 +11,16 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/caqti-study/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'benjamin-thomas', // Usually your GitHub org/user name.
   projectName: 'caqti-study', // Usually your repo name.
+  trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warn', // TODO: use throw
+  onBrokenMarkdownLinks: 'warn', // TODO: use throw
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
