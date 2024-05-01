ca6bbf60d5d142f3a71c6deff920371182fb489e
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-05-01 09:52:16 +0200

    Slightly improve the bikereg examples (#17)

--- study/bikereg-eio/bikereg.ml
+++ study/bikereg-eio/bikereg.ml
@@ -17,18 +17,18 @@
      stolen: Ptime.t option;
    }
  end
- 
+
  (* Query Strings
   * =============
   *
   * Queries are normally defined in advance.  This allows Caqti to emit
   * prepared queries which are reused throughout the lifetime of the
   * connection. *)
- 
+
  module Q = struct
    open Caqti_request.Infix
    open Caqti_type.Std
- 
+
    let bike =
      let open Bike in
      let intro frameno owner stolen = {frameno; owner; stolen} in
@@ -37,7 +37,7 @@
        @@ proj string (fun bike -> bike.owner)
        @@ proj (option ptime) (fun bike -> bike.stolen)
        @@ proj_end
- 
+
    let create_bikereg =
      unit ->. unit @@
      {eos|
@@ -47,31 +47,32 @@
          stolen timestamp NULL
        )
      |eos}
- 
+
    let reg_bike =
      t2 string string ->. unit @@
      "INSERT INTO bikereg (frameno, owner) VALUES (?, ?)"
- 
+
    let report_stolen =
-     string ->. unit @@
-     "UPDATE bikereg SET stolen = current_timestamp WHERE frameno = ?"
- 
+    t2 string string  ->. unit @@
+     "UPDATE bikereg SET stolen = ? WHERE frameno = ?"
+
+
    let select_stolen =
      unit ->* bike @@
      "SELECT * FROM bikereg WHERE NOT stolen IS NULL"
- 
+
    let select_owner =
      string ->? string @@
      "SELECT owner FROM bikereg WHERE frameno = ?"
  end
- 
+
  (* Wrappers around the Generic Execution Functions
   * ===============================================
   *
   * Here we combine the above queries with a suitable execution function, for
   * convenience and to enforce type safety.  We could have defined these in a
   * functor on CONNECTION and used the resulting module in place of Db. *)
- 
+
  (* Db.exec runs a statement which must not return any rows.  Errors are
   * reported as exceptions. *)
  let create_bikereg (module Db : Caqti_eio.CONNECTION) =
@@ -79,26 +80,26 @@
  let reg_bike (module Db : Caqti_eio.CONNECTION) frameno owner =
    Db.exec Q.reg_bike (frameno, owner)
  let report_stolen (module Db : Caqti_eio.CONNECTION) frameno =
-   Db.exec Q.report_stolen frameno
- 
+   Db.exec Q.report_stolen ("2024-05-01-T09:05:00", frameno)
+
  (* Db.find runs a query which must return at most one row.  The result is a
   * option, since it's common to seach for entries which don't exist. *)
  let find_bike_owner frameno (module Db : Caqti_eio.CONNECTION) =
    Db.find_opt Q.select_owner frameno
- 
+
  (* Db.iter_s iterates sequentially over the set of result rows of a query. *)
  let iter_s_stolen (module Db : Caqti_eio.CONNECTION) f =
    Db.iter_s Q.select_stolen f ()
- 
+
  (* There is also a Db.iter_p for parallel processing, and Db.fold and
   * Db.fold_s for accumulating information from the result rows. *)
- 
- 
+
+
  (* Test Code
   * ========= *)
- 
+
  let (>>=) r f = Result.bind r f
- 
+
  let test stdout db =
    (* Examples of statement execution: Create and populate the register. *)
    create_bikereg db >>= fun () ->
@@ -109,7 +110,7 @@
    reg_bike db "BIKE-0004" "Marvin" >>= fun () ->
    report_stolen db "BIKE-0000" >>= fun () ->
    report_stolen db "BIKE-0004" >>= fun () ->
- 
+
    (* Examples of single-row queries. *)
    let show_owner frameno =
      find_bike_owner frameno db >>= fun owner_opt ->
@@ -120,7 +121,7 @@
     in
    show_owner "BIKE-0003" >>= fun () ->
    show_owner "BIKE-0042" >>= fun () ->
- 
+
    (* An example multi-row query. *)
    Flow.copy_string "Stolen:" stdout;
    iter_s_stolen db
@@ -134,7 +135,7 @@
           ) stdout;
           Ok ()
      )
- 
+
   let () = Eio_main.run @@ fun env ->
     let url =
       Printf.sprintf "postgresql://%s%s%s%s"
---
+++ study/bikereg-lwt/README.md
@@ -0,0 +1,16 @@
+# Bike registration example (over lwt)
+
+This simple program demonstrates:
+
+- creating a temporary table
+- inserting rows
+- selecting rows
+- deleting rows
+
+```sh
+$ PGHOST=localhost PGDATABASE=caqti_study PGPORT=5433 ./bikereg.exe
+BIKE-0003 is owned by Trillian.
+BIKE-0042 is not registered.
+Stolen: BIKE-0000 2024-05-01T09:05:00-00:00 Arthur Dent
+        BIKE-0004 2024-05-01T09:05:00-00:00 Marvin
+```
